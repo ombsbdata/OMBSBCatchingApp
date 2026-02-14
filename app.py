4dcea2a605f29d9d20eba24bf9ce131a8d2c11e4
ombsbdata <ombsbdata@gmail.com>
2026-02-13 19:48:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ df_fawley = pd.read_csv(fawley_csv_path, usecols=[c for c in rebs_columns_needed
 df_sec = df_sec[df_sec['PitchCall'].isin(['StrikeCalled', 'BallCalled'])]
 df_fawley = df_fawley[df_fawley['PitchCall'].isin(['StrikeCalled', 'BallCalled'])]
 
-df_sec = df_sec[df_sec['game_type'].isin(['IS','FallGame','REG'])]
-df_fawley = df_fawley[df_fawley['game_type'].isin(['IS','FallGame','REG'])]
+df_sec = df_sec[df_sec['game_type'].isin(['IS','FallGame','Reg'])]
+df_fawley = df_fawley[df_fawley['game_type'].isin(['IS','FallGame','Reg'])]
 
 
 # Dates → datetime
